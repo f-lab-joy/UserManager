faaaa4b8b2f98dcf181e6464f11b8cb58266ea38
greglee <greg@29cm.co.kr>
2022-02-12 13:32:17 +0900

    init project

---
+++ src/main/java/kr/flab/UserManager/UserManagerApplication.java
@@ -0,0 +1,13 @@
+package kr.flab.UserManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagerApplication.class, args);
+	}
+
+}
